fc34647f6ea22d3ec8c7bb10f26cf98d58315ccd
Justin Clayton <jt.clayton92@yahoo.com>
2020-07-15 13:54:16 -0700

    Set up routes and views for basic navigation

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'app'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('wall/', views.wall, name='wall'),
+    path('messages/create', views.create_message, name='create_message'),
+    path('messages/<int:message_id>/destroy', views.destroy_message, name='destroy_message'),
+    path('comments/create', views.create_comment, name='create_comment'),
+    path('comments/<int:comment_id>/destroy', views.destroy_comment, name='destroy_comment'),
+    path('logout', views.logout, name='logout')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+
 def index(request):
   return render(request, 'index.html')
+
+def login(request):
+  return redirect(reverse('app:wall'))
+
+def wall(request):
+  return render(request, 'wall.html')
+
+def create_message(request):
+  return redirect(reverse('app:wall'))
+
+def destroy_message(request, message_id):
+  return redirect(reverse('app:wall'))
+
+def create_comment(request):
+  return redirect(reverse('app:wall'))
+
+def destroy_message(request):
+  return redirect(reverse('app:wall'))
+
+def logout(request):
+  request.session.clear()
+  return redirect(reverse('app:index'))
