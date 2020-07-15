73a5104b027d0e25d4afc00be8962f26523a2d92
Justin Clayton <jt.clayton92@yahoo.com>
2020-07-14 17:38:04 -0700

    Add user, message, and comment models. Migrate DB.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+
+class User(BaseModel):
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
+  pw_hash = models.CharField(max_length=255)
+
+  def __str__(self):
+    return f'User {self.first_name} {self.last_name}'
+
+
+class Message(BaseModel):
+  user_id = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name='messages')
+  text = models.TextField()
+
+  def __str__(self):
+    return f'Message {self.id}'
+
+
+class Comment(BaseModel):
+  message_id = models.ForeignKey(
+    Message, on_delete=models.CASCADE, related_name='comments')
+  user_id = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name='comments')
+  text = models.TextField()
+
+  def __str__(self):
+    return f'Comment {self.id} on {self.message_id}'
